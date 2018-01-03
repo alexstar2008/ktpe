5029c99b19d19c7766e201d059792de859e45027
alexstar2008 <alexstar2008@ukr.net>
2018-01-04 01:15:03 +0200

    quick

--- resources/views/pages/conferences.blade.php
+++ resources/views/pages/conferences.blade.php
@@ -1,5 +1,5 @@
 @extends('pages.app')
-@section('title',__('activity.seo_title'))
+@section('title',__('conferences.seo_title'))
 
 @section('content')
     <div class="container" style="margin-top: 20px;">
